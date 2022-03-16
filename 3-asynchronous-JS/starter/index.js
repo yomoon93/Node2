2d86e8d898e80080dc73ec88aa40908f5e8fda8e
Kevin <kevmunar@hotmail.com>
2022-03-16 16:23:12 -0400

    added parts

--- 3-asynchronous-JS/starter/index.js
+++ 3-asynchronous-JS/starter/index.js
@@ -1,12 +1,53 @@
-const fs = require("fs");
-const superagent = require("superagent");
+const fs = require('fs');
 
-fs.readFile(`${__dirname}/dog.txt`, (err, data) => {
-  console.log(`Breed ${data}`);
+const superagent = require('superagent');
 
-  superagent
-    .get(`https://dog.ceo/api/breed/${data}/images/random`)
-    .end((err, res) => {
-      console.log(res.body);
+const readFilePro = (file) => {
+  return new Promise((resolve, reject) => {
+    fs.readFile(file, (err, data) => {
+      //whatever we pass into the function what will later be available as the argument in the then
+      if (err) reject();
+      // whatever we pass  in here is what will  be available as the argument in the catch
+      resolve(data);
     });
-});
+  });
+};
+
+const writeFilePro = (file, data) => {
+  return new Promise((resolve, reject) => {
+    fs.writeFile(file, data, (err) => {
+        if (err) reject('Could not write file');
+        resolve('success');
+      });
+  });
+};
+
+
+
+//returns a promise we can use a then
+readFilePro(`${__dirname}/dog.txt`)
+  .then(data => {
+    console.log(`Breed ${data}`);
+// we did some chaining so each handler returns a new promise
+    return superagent.get(`https://dog.ceo/api/breed/${data}/images/random`);
+  })
+  .then((res) => {
+    console.log(res.body.message);
+
+
+    return writeFilePro('dog-img.txt', res.body.message)
+//old way of doing any of the work
+  //   fs.writeFile('dog-img.txt', res.body.message, (err) => {
+  //     if (err) return console.log(err.message);
+  //     console.log('Random dog image saved file!');
+  //   });
+   })
+
+   .then(()=>{
+     console.log('Random dog image save to file!')
+   })
+  .catch((err) => {
+    console.log(err.message);
+  });
+
+// CAllBACK HELL SECTION
